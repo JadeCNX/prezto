c31a44653c3da5d37c830f4c34c7b9ec9b089df9
jade <jadearmy89@gmail.com>
2021-07-17 13:35:34 +0700

    Update alias

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -224,6 +224,7 @@ fi
 # Miscellaneous
 
 alias c='clear'
+alias swagger="docker run --rm -it  --user $(id -u):$(id -g) -e GOPATH=$HOME/go:/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger"
 
 if type "vtop" > /dev/null; then
   alias vtop='vtop --theme wizard'
