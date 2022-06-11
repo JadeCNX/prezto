aeab312e0ea45fb2b2dbfe8acaf955d1121a8ddf
jade <jadearmy89@gmail.com>
2022-06-12 00:46:13 +0700

    feat: update osx alias

--- modules/osx/init.zsh
+++ modules/osx/init.zsh
@@ -27,5 +27,5 @@ alias pushdf='pushd "$(pfd)"'
 alias hidden-show='defaults write com.apple.finder AppleShowAllFiles -bool YES && killall Finder'
 alias hidden-hide='defaults write com.apple.finder AppleShowAllFiles -bool NO && killall Finder'
 
-alias airport /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
-alias afk 'open -a /System/Library/CoreServices/ScreenSaverEngine.app'
+alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+alias afk='open -a /System/Library/CoreServices/ScreenSaverEngine.app'
