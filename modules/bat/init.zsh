3c9dad979c9546ebeef19b3fb5bb34bb53feeeab
jade <jadearmy89@gmail.com>
2022-08-04 15:33:57 +0700

    feat: add bat module

---
+++ modules/bat/init.zsh
@@ -0,0 +1,19 @@
+#
+# A cat(1) clone with syntax highlighting and Git integration.
+# https://github.com/sharkdp/bat
+#
+
+if type "bat" > /dev/null; then
+  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  export PAGER="bat --style='numbers,changes'"
+  alias cat="bat -pp"
+
+  help() {
+      "$@" --help 2>&1 | bat --plain --language=help
+  }
+  alias h='help'
+fi
+
+gdB() {
+    git diff --name-only --relative --diff-filter=d | xargs bat --style=full --diff
+}
