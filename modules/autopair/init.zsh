07c0069b88df91d0bf3f877ec3fbee066916dcfa
jade <jadearmy89@gmail.com>
2024-03-23 23:15:30 +0700

    feat: add autopair for zsh

---
+++ modules/autopair/init.zsh
@@ -0,0 +1,9 @@
+#
+# A simple plugin that auto-closes, deletes and skips over matching delimiters in zsh intelligently
+# https://github.com/hlissner/zsh-autopair
+#
+
+if [[ -e $(brew --prefix)/share/zsh-autopair/autopair.zsh ]]; then
+  echo "autopair"
+  source $(brew --prefix)/share/zsh-autopair/autopair.zsh
+fi
